03a899becaba0745fa4fc56386d73fe42a66d011
danny shdeed <daniel_shdeed@hotmail.com>
2021-10-04 15:54:26 +0300

    Fix Linter errors

--- javascript.js
+++ javascript.js
@@ -4,25 +4,20 @@ const rightsect = document.getElementById('rightsection');
 const timesect = document.getElementById('timesection');
 const closebtn = document.getElementById('close-btn');
 
-
 menubtn.addEventListener('click', () => {
-  
-   navlist.classList.toggle('show');
-   timesect.classList.toggle('hide');
-   rightsect.classList.toggle('hide');
-  
-})
+  navlist.classList.toggle('show');
+  timesect.classList.toggle('hide');
+  rightsect.classList.toggle('hide');
+});
 
 navlist.addEventListener('click', () => {
-   navlist.classList.toggle('show');
-   rightsect.classList.toggle('hide');
-   timesect.classList.toggle('hide');
- 
-})
+  navlist.classList.toggle('show');
+  rightsect.classList.toggle('hide');
+  timesect.classList.toggle('hide');
+});
 
 closebtn.addEventListener('click', () => {
-    navlist.classList.toggle('show');
-   rightsect.classList.toggle('hide');
-   timesect.classList.toggle('hide');
-})
-
+  navlist.classList.toggle('show');
+  rightsect.classList.toggle('hide');
+  timesect.classList.toggle('hide');
+});
