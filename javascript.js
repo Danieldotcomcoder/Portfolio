b12a4d3fbfee3d1532e68be75b5dc11d40a6ceea
danny shdeed <daniel_shdeed@hotmail.com>
2021-10-04 14:02:10 +0300

    Add mobile menu functionality

--- javascript.js
+++ javascript.js
@@ -1,12 +1,30 @@
 const menubtn = document.getElementById('menu-btn');
 const navlist = document.getElementById('navbar-list');
+const rightsect = document.getElementById('rightsection');
+const timesect = document.getElementById('timesection');
+
+
 const closebtn = document.getElementById('close-btn');
 
 
 menubtn.addEventListener('click', () => {
+  
    navlist.classList.toggle('show');
+   timesect.classList.toggle('hide');
+   rightsect.classList.toggle('hide');
+  
+})
+
+navlist.addEventListener('click', () => {
+   navlist.classList.toggle('show');
+   rightsect.classList.toggle('hide');
+   timesect.classList.toggle('hide');
+ 
 })
 
 closebtn.addEventListener('click', () => {
    navlist.classList.toggle('show');
+   rightsect.classList.toggle('hide');
+   timesect.classList.toggle('hide');
 })
+
