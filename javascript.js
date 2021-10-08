28c8f61c7e73f99d71b89edaec191121fa6a1246
danny shdeed <daniel_shdeed@hotmail.com>
2021-10-08 22:25:33 +0300

    Add Javascript and style for popup-window Desktop

--- javascript.js
+++ javascript.js
@@ -73,7 +73,7 @@ for (let i = 0; i <= 3; i++) {
   technologies[i].appendChild(jsimg.cloneNode(true));
 }
 
-const projectinformation =
+const projectinformation = [               
 {
   title: "Tonic",
   Description: "<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea</p> ",
@@ -81,20 +81,51 @@ const projectinformation =
   tech1: "img/html.png",
   tech2: "img/css.png",
   tech3: "img/javascript.png",
-  link: 'https://www.google.com',
+  link: "#",
   source: "#",
-}
+},
+{
+  title: "Multi-Post Stories ",
+  Description: "<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea</p> ",
+  image: "img/desktop/1.png",
+  tech1: "img/html.png",
+  tech2: "img/css.png",
+  tech3: "img/javascript.png",
+  link: "#",
+  source: "#",
+},
+{
+  title: "Tonic",
+  Description: "<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea</p> ",
+  image: "img/desktop/2.png",
+  tech1: "img/html.png",
+  tech2: "img/css.png",
+  tech3: "img/javascript.png",
+  link: "#",
+  source: "#",
+},
+{
+  title: "Multi-Post Stories ",
+  Description: "<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea</p> ",
+  image: "img/desktop/3.png",
+  tech1: "img/html.png",
+  tech2: "img/css.png",
+  tech3: "img/javascript.png",
+  link: "#",
+  source: "#",
+},
+]
 
-
-const popupmodal = document.querySelector('.popup-modal');
+const popupmodal = document.querySelectorAll('.popup-modal');
 
 window.addEventListener('DOMContentLoaded', () => {
-  popupmodal.innerHTML =
+  popupmodal[0].innerHTML =
     `<div class="popup">
         <div class="popup-info-block">
             <div class="popup-primary-text-box">
             <div class="popup-project-title">
-            ${projectinformation.title}
+            ${projectinformation[0].title}
+            <img class="popup-closebtn" onclick="closepopup1()" src="img/Disabled.png" alt="close-button" width="44" height="45">
           </div>
           <div class="popup-project-frame">
               <h3>CANOPY</h3>
@@ -105,37 +136,235 @@ window.addEventListener('DOMContentLoaded', () => {
           </div>
       </div>
       <div class="popupsnapshot">
-        <img src=${projectinformation.image} alt="pop-image" >
+        <img src=${projectinformation[0].image} alt="pop-image" >
       </div>
       <div class="popup-project-description" id="proj-description">
-      ${projectinformation.Description}
+      ${projectinformation[0].Description}
       </div>
       <div class="popup-project-tags">
       <ul>
-         <li><img src=${projectinformation.tech1}> </li>
-         <li><img src=${projectinformation.tech2}> </li>
-         <li><img src=${projectinformation.tech3}> </li>
+         <li><img src=${projectinformation[0].tech1}> </li>
+         <li><img src=${projectinformation[0].tech2}> </li>
+         <li><img src=${projectinformation[0].tech3}> </li>
       </ul>
       </div>
       <div class="popup-project-buttons">
-      <button onclick="window.location.href=${projectinformation.link};">&nbsp&nbsp&nbsp&nbsp  See live <img src="img/btn-img-live.png" width="15" height="15" ></button>
-      <button onclick="window.location.href=${projectinformation.source};">&nbsp&nbsp&nbsp&nbsp  See Source <img src="img/btn-img-src.png" width="15" height="15"></button>
+      <a href=${projectinformation[0].link}> <button>&nbsp&nbsp&nbsp&nbsp  See live <img src="img/btn-img-live.png" width="15" height="15" ></button></a>
+      <a href=${projectinformation[0].source}> <button>&nbsp&nbsp&nbsp&nbsp  See Source <img src="img/btn-img-src.png" width="15" height="15"></button></a>
       </div>
   </div>`;
 })
 
-function  openpopup() {
-  var x = document.querySelector('.popup');
-  if (x.style.display === "flex") {
-    x.style.display = "none";
+function  openpopup1() {
+  var  x = document.querySelectorAll('.popup');
+  if (x[0].style.display === "flex") {
+    x[0].style.display = "none";
   }  else {
-    x.style.display = "flex";
+    x[0].style.display = "flex";
   }
+   window.scroll({top:650});
 }
-const seeprojectbutton = document.querySelector(".seeproject");
-const workssection = document.querySelector(".card-works");
-//const popupwindow = document.querySelector('.popup-modal');
-seeprojectbutton.addEventListener('click', () => {
-     openpopup();
-      workssection.classList.toggle('hide');
+
+function  closepopup1() {
+  var  y = document.querySelectorAll('.popup');
+ if (y[0].style.display === "flex") {
+    y[0].style.display = "none";
+  }  else {
+    y[0].style.display = "flex";
+  }
+  workssection[0].classList.toggle('hide');
+}
+
+const seeprojectbutton = document.querySelectorAll(".seeproject");
+const workssection = document.querySelectorAll(".card-works");
+
+seeprojectbutton[0].addEventListener('click', () => {
+     openpopup1();
+      workssection[0].classList.toggle('hide');
+});
+
+
+
+window.addEventListener('DOMContentLoaded', () => {
+  popupmodal[1].innerHTML =
+    `<div class="popup">
+        <div class="popup-info-block">
+            <div class="popup-primary-text-box">
+            <div class="popup-project-title">
+            ${projectinformation[1].title}
+            <img class="popup-closebtn1" onclick="closepopup2()" src="img/Disabled.png" alt="close-button" width="44" height="45">
+          </div>
+          <div class="popup-project-frame">
+              <h3>CANOPY</h3>
+              <img src="img/Counter.png" alt="counter" width="8" height="8">
+              <h4>Back End Dev</h4>
+              <img src="img/Counter1.png" alt="counter1" width="8" height="8">
+              <h4>2015</h4>
+          </div>
+      </div>
+      <div class="popupsnapshot">
+        <img src=${projectinformation[1].image} alt="pop-image" >
+      </div>
+      <div class="popup-project-description" id="proj-description">
+      ${projectinformation[1].Description}
+      </div>
+      <div class="popup-project-tags">
+      <ul>
+         <li><img src=${projectinformation[1].tech1}> </li>
+         <li><img src=${projectinformation[1].tech2}> </li>
+         <li><img src=${projectinformation[1].tech3}> </li>
+      </ul>
+      </div>
+      <div class="popup-project-buttons">
+      <a href=${projectinformation[1].link}> <button>&nbsp&nbsp&nbsp&nbsp  See live <img src="img/btn-img-live.png" width="15" height="15" ></button></a>
+      <a href=${projectinformation[1].source}> <button>&nbsp&nbsp&nbsp&nbsp  See Source <img src="img/btn-img-src.png" width="15" height="15"></button></a>
+      </div>
+  </div>`;
+})
+function  openpopup2() {
+  var  x = document.querySelectorAll('.popup');
+  if (x[1].style.display === "flex") {
+    x[1].style.display = "none";
+  }  else {
+    x[1].style.display = "flex";
+  }
+   window.scroll({top:1280});
+}
+
+function  closepopup2() {
+  var  y = document.querySelectorAll('.popup');
+ if (y[1].style.display === "flex") {
+    y[1].style.display = "none";
+  }  else {
+    y[1].style.display = "flex";
+  }
+  workssection[1].classList.toggle('hide');
+}
+
+seeprojectbutton[1].addEventListener('click', () => {
+     openpopup2();
+      workssection[1].classList.toggle('hide');
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+  popupmodal[2].innerHTML =
+    `<div class="popup">
+        <div class="popup-info-block">
+            <div class="popup-primary-text-box">
+            <div class="popup-project-title">
+            ${projectinformation[2].title}
+            <img class="popup-closebtn" onclick="closepopup3()" src="img/Disabled.png" alt="close-button" width="44" height="45">
+          </div>
+          <div class="popup-project-frame">
+              <h3>CANOPY</h3>
+              <img src="img/Counter.png" alt="counter" width="8" height="8">
+              <h4>Back End Dev</h4>
+              <img src="img/Counter1.png" alt="counter1" width="8" height="8">
+              <h4>2015</h4>
+          </div>
+      </div>
+      <div class="popupsnapshot">
+        <img src=${projectinformation[2].image} alt="pop-image" >
+      </div>
+      <div class="popup-project-description" id="proj-description">
+      ${projectinformation[2].Description}
+      </div>
+      <div class="popup-project-tags">
+      <ul>
+         <li><img src=${projectinformation[2].tech1}> </li>
+         <li><img src=${projectinformation[2].tech2}> </li>
+         <li><img src=${projectinformation[2].tech3}> </li>
+      </ul>
+      </div>
+      <div class="popup-project-buttons">
+      <a href=${projectinformation[2].link}> <button>&nbsp&nbsp&nbsp&nbsp  See live <img src="img/btn-img-live.png" width="15" height="15" ></button></a>
+      <a href=${projectinformation[2].source}> <button>&nbsp&nbsp&nbsp&nbsp  See Source <img src="img/btn-img-src.png" width="15" height="15"></button></a>
+      </div>
+  </div>`;
+})
+function  openpopup3() {
+  var  x = document.querySelectorAll('.popup');
+  if (x[2].style.display === "flex") {
+    x[2].style.display = "none";
+  }  else {
+    x[2].style.display = "flex";
+  }
+   window.scroll({top:1900});
+}
+
+function  closepopup3() {
+  var  y = document.querySelectorAll('.popup');
+ if (y[2].style.display === "flex") {
+    y[2].style.display = "none";
+  }  else {
+    y[2].style.display = "flex";
+  }
+  workssection[2].classList.toggle('hide');
+}
+
+seeprojectbutton[2].addEventListener('click', () => {
+     openpopup3();
+      workssection[2].classList.toggle('hide');
+});
+
+
+window.addEventListener('DOMContentLoaded', () => {
+  popupmodal[3].innerHTML =
+    `<div class="popup">
+        <div class="popup-info-block">
+            <div class="popup-primary-text-box">
+            <div class="popup-project-title">
+            ${projectinformation[3].title}
+            <img class="popup-closebtn1" onclick="closepopup4()" src="img/Disabled.png" alt="close-button" width="44" height="45">
+          </div>
+          <div class="popup-project-frame">
+              <h3>CANOPY</h3>
+              <img src="img/Counter.png" alt="counter" width="8" height="8">
+              <h4>Back End Dev</h4>
+              <img src="img/Counter1.png" alt="counter1" width="8" height="8">
+              <h4>2015</h4>
+          </div>
+      </div>
+      <div class="popupsnapshot">
+        <img src=${projectinformation[3].image} alt="pop-image" >
+      </div>
+      <div class="popup-project-description" id="proj-description">
+      ${projectinformation[3].Description}
+      </div>
+      <div class="popup-project-tags">
+      <ul>
+         <li><img src=${projectinformation[3].tech1}> </li>
+         <li><img src=${projectinformation[3].tech2}> </li>
+         <li><img src=${projectinformation[3].tech3}> </li>
+      </ul>
+      </div>
+      <div class="popup-project-buttons">
+      <a href=${projectinformation[3].link}> <button>&nbsp&nbsp&nbsp&nbsp  See live <img src="img/btn-img-live.png" width="15" height="15" ></button></a>
+      <a href=${projectinformation[3].source}> <button>&nbsp&nbsp&nbsp&nbsp  See Source <img src="img/btn-img-src.png" width="15" height="15"></button></a>
+      </div>
+  </div>`;
+})
+function  openpopup4() {
+  var  x = document.querySelectorAll('.popup');
+  if (x[3].style.display === "flex") {
+    x[3].style.display = "none";
+  }  else {
+    x[3].style.display = "flex";
+  }
+   window.scroll({top:2500});
+}
+
+function  closepopup4() {
+  var  y = document.querySelectorAll('.popup');
+ if (y[3].style.display === "flex") {
+    y[3].style.display = "none";
+  }  else {
+    y[3].style.display = "flex";
+  }
+  workssection[3].classList.toggle('hide');
+}
+
+seeprojectbutton[3].addEventListener('click', () => {
+     openpopup4();
+      workssection[3].classList.toggle('hide');
 });
